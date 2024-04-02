9f7280a63e500f8a248f1659749b8a47acf5eec6
Yuri <wall.mart.boss@proton.me>
2024-04-02 18:11:50 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,15 @@
-let quantity;
-let pricePerDroid;
-let customerCredits;
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-	let amount;
-	amount = quantity * pricePerDroid;
-	if (amount > customerCredits) {
-		return `Insufficient funds!`;
-	} else {
-		return `You ordered ${quantity} droids worth ${amount} credits!`;
+function slugify(title) {
+	let array_title = title.split(" ");
+	let array_new = [];
+	for (let i = 0; i < array_title.length; i += 1) {
+		let item = array_title[i].toLowerCase();
+		array_new.push(item);
 	}
+	const arr = array_new.join("-");
+	return arr;
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
